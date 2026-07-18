7bf4b10e97f7c3b52ad9d74e10fd6fe1380ca28a
lvnttran <72700114+lvnttran@users.noreply.github.com>
2026-07-18 07:46:40 +0000

    Deploying to gh-pages from @ lvnttran/tum_gitcv@16baf7a8737c5efbee43cc65807f55b9d00e2c5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,21 +501,35 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/tum_gitcv/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/tum_gitcv/assets/pdf/TRAN_CV_26.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%65-%76%69-%6E%68%61%6E-%74%61%6D.%74%72%61%6E@%75%6E%69%76-%67%72%65%6E%6F%62%6C%65-%61%6C%70%65%73.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/lvnttran", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/tlvntam", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0007-2574-8110", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,7 +543,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ym5GhGoAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
